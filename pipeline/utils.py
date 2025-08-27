d319f7c0b2e2d99a9f7a779db6d652325c634635
Matheus Oscar <matheus.oscar@hotmail.com>
2025-08-27 17:13:52 -0300

    revisao_pd

--- pipeline/utils.py
+++ pipeline/utils.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 from feature_engine.encoding import OneHotEncoder
+import matplotlib.pyplot as plt
 
 
 def diagnostico_abt(abt, target="atraso_90d"):
@@ -211,7 +212,7 @@ def comparar_ks(df_M, df_M1, feature, target="atraso_90d", bins=10):
     }
 
 
-def remover_vars(abt, target="atraso_90d", iv_threshold=0.01, corr_threshold=0.8):
+def remover_vars(abt, target="atraso_90d", iv_threshold=0.01, corr_threshold=0.8, cols_drop=[]):
     ivs = {}
     for col in abt.columns:
         if col in [target, 'estado_civil'] + cols_drop:
@@ -288,3 +289,152 @@ def plotar_ks(y_true, y_pred_proba, titulo="KS Curve"):
     plt.show()
 
     return ks_val, score_ks
+
+
+def plot_categ(df, column):
+    # Plotar um barra de contagem de categoria
+    aux = df.groupby(column)[column].count().reset_index(name='Qtd.')
+    aux.plot.bar(x=column, y='Qtd.', rot=0, figsize=(12, 4))
+
+
+def plot_txmau_categ(df, column, column_mau, mau=1):
+    # usando mau = 1
+    df2 = df[[column, column_mau]].copy()
+    df2['mau'] = [1 if x == mau else 0 for x in df2[column_mau]]
+    aux = df2.groupby(column)["mau"].agg(["mean", 'count'])
+    aux = aux.rename(
+        columns={'mean': 'Tx. inadimplência', 'count': 'Volumetria'})
+    aux.plot.bar(rot=45, subplots=True, figsize=(12, 4), fontsize=8)
+
+
+def ks(data=None, target=None, prob=None, printar=False, return_ks=False):
+    # Calcular KS
+
+    # Copiar a base para não influenciar na base original (data)
+    data = data[[target, prob]].copy()
+
+    # target = event; target0 = non_event
+    data['target0'] = 1 - data[target]
+    data['bucket'] = pd.qcut(data[prob], 10)
+    grouped = data.groupby('bucket', as_index=False)
+    kstable = pd.DataFrame()
+    kstable['min_prob'] = grouped.min()[prob]
+    kstable['max_prob'] = grouped.max()[prob]
+    kstable['events'] = grouped.sum()[target]
+    kstable['non_events'] = grouped.sum()['target0']
+    kstable = kstable.sort_values(
+        by="min_prob", ascending=False).reset_index(drop=True)
+    kstable['event rate'] = (
+        kstable.events / data[target].sum()).apply('{0:.2%}'.format)
+    kstable['non_event rate'] = (
+        kstable.non_events / data['target0'].sum()).apply('{0:.2%}'.format)
+    kstable['event cum_rate'] = (kstable.events / data[target].sum()).cumsum()
+    kstable['non_event cum_rate'] = (
+        kstable.non_events / data['target0'].sum()).cumsum()
+    kstable['KS'] = abs(np.round(kstable['event cum_rate'] *
+                        100 - kstable['non_event cum_rate'] * 100, 1))  # * 100
+
+    kstable['n'] = kstable['events'] + kstable['non_events']
+    kstable['Class Event Rate'] = np.round(
+        kstable.events / kstable.n, 3).apply('{0:.2%}'.format)
+    kstable['%'] = (kstable['n'] / kstable['n'].sum()).apply('{0:.0%}'.format)
+
+    # Formating
+    kstable['event cum_rate'] = kstable['event cum_rate'].apply(
+        '{0:.2%}'.format)
+    kstable['non_event cum_rate'] = kstable['non_event cum_rate'].apply(
+        '{0:.2%}'.format)
+    kstable.index = range(1, 11)
+    kstable.index.rename('Decile', inplace=True)
+    pd.set_option('display.max_columns', 12)
+    if printar:
+        print(kstable)
+
+    if return_ks:
+        return (max(kstable['KS']))
+
+    # Display KS
+    from colorama import Fore
+    print(Fore.RED +
+          "KS is " +
+          str(max(kstable['KS'])) + "%" +
+          " at decile " +
+          str((kstable.index[kstable['KS'] == max(kstable['KS'])][0])))
+    return (kstable)
+
+
+def get_precisions_recalls(actual, preds):
+    plt.figure(figsize=(16, 4))
+
+    plt.subplot(1, 2, 1)
+    precision_0 = np.sum((actual == 0) & (preds == 0)) / np.sum(preds == 0)
+    precision_1 = np.sum((actual == 1) & (preds == 1)) / np.sum(preds == 1)
+
+    plt.bar([0, 1], [precision_0, precision_1])
+    plt.xticks([0, 1], ['Class 0', 'Class 1'], fontsize=20)
+    plt.yticks(np.arange(0, 1.1, 0.1), fontsize=14)
+    plt.ylabel('Precision', fontsize=20)
+    plt.title(
+        f'Precision Class 0: {round(precision_0, 2)}\nPrecision Class 1: {round(precision_1, 2)}', fontsize=20)
+
+    plt.subplot(1, 2, 2)
+    recall_0 = np.sum((actual == 0) & (preds == 0)) / np.sum(actual == 0)
+    recall_1 = np.sum((actual == 1) & (preds == 1)) / np.sum(actual == 1)
+
+    plt.bar([0, 1], [recall_0, recall_1])
+    plt.xticks([0, 1], ['Class 0', 'Class 1'], fontsize=20)
+    plt.yticks(np.arange(0, 1.1, 0.1), fontsize=14)
+    plt.ylabel('Recall', fontsize=20)
+    plt.title(
+        f'Recall Class 0: {round(recall_0, 2)}\nRecall Class 1: {round(recall_1, 2)}', fontsize=20)
+
+    plt.tight_layout()
+    plt.show()
+
+
+def taxa_inadimplencia_por_variavel(df, var, target="atraso_90d", bins=10):
+    """
+    Calcula a taxa de inadimplência por faixas de uma variável contínua.
+    """
+    df = df.copy()
+    df["faixa"] = pd.qcut(df[var], q=bins, duplicates="drop")
+
+    taxa = (
+        df.groupby("faixa")
+        .agg(
+            n=(target, "size"),
+            taxa_inadimplencia=(target, "mean")
+
+        )
+        .reset_index()
+    )
+    return taxa
+
+
+def plot_inad_var(df, var, target="atraso_90d", bins=10):
+    """
+    Plota a taxa de inadimplência por faixas de uma variável contínua.
+    Mostra também o nº de observações em cada faixa.
+    """
+    # Calcula as taxas
+    taxa = taxa_inadimplencia_por_variavel(df, var, target, bins)
+
+    # Cria o gráfico
+    plt.figure(figsize=(10, 5))
+    plt.plot(taxa["faixa"].astype(str), taxa["taxa_inadimplencia"],
+             marker="o", label="Taxa de inadimplência")
+    plt.xticks(rotation=45)
+    plt.ylabel("Taxa de Inadimplência")
+    plt.title(f"Taxa de inadimplência por {var}")
+
+    # Adiciona rótulos de n_observacoes
+    for i, row in taxa.iterrows():
+        plt.text(i, row["taxa_inadimplencia"]+0.01, str(row["n"]),
+                 ha="center", fontsize=8, color="black")
+
+    plt.legend()
+    plt.grid(alpha=0.3)
+    plt.tight_layout()
+    plt.show()
+
+    return taxa
